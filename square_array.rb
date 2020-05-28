35707ec3f36b7ee8d5ad59d24010b4a25ed03d03
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-28 19:17:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,16 @@
+# def square_array(array)
+#   new_array = []
+#   array.each do |number|
+#     squared_number = number**2
+#     new_array << squared_number
+#   end
+#   return new_array
+# end
+
+
 def square_array(array)
-  new_array = []
-  array.each do |number|
+  array.collect do |number|
     squared_number = number**2
-    new_array << squared_number
   end
-  return new_array
+  return array
 end
